d2368910d97a5f5f1029aaf2feb375fe91e58e78
Kavika <kbpalletenne@gmail.com>
2021-04-12 12:22:18 +1000

    find by password reset token functionality

--- src/main/java/xyz/scantag/dev/api/persistence/UserRepository.java
+++ src/main/java/xyz/scantag/dev/api/persistence/UserRepository.java
@@ -11,5 +11,6 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
+    Optional<User> findByResetPasswordToken(String token);
 
 }
